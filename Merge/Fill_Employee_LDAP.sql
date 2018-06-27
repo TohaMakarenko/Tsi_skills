4260105c5f617f180f4725815854ddff1c84da24
Toha Makarenko <demo6112@gmail.com>
2018-06-27 13:05:48 +0300

    merge

--- Merge/Fill_Employee_LDAP.sql
+++ Merge/Fill_Employee_LDAP.sql
@@ -19,6 +19,13 @@ USING (SELECT
 		WHERE [SUIR1].[SysUserId] = [User].[Id]
 			AND [Role].[SysAdminUnitTypeValue] = 1
 	) Role
+	WHERE [User].[SysAdminUnitTypeValue] = 4
+		AND NOT EXISTS (SELECT
+				NULL
+			FROM [SysUserInRole] AS [NotPortalFilter]
+			WHERE [User].[Id] = [NotPortalFilter].[SysUserId]
+				AND [NotPortalFilter].[SysRoleId] = '720b771c-e7a7-4f31-9cfb-52cd21c3739f'
+		)
 ) src
 ON trg.[ContactId] = src.[ContactId]
 WHEN MATCHED
